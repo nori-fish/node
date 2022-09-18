ea8b318e5a3ef392b658e46a05b2ae156dc026e1
Jim O'Brien <git@jimmed.net>
2022-09-18 13:50:23 +0100

    fix: only attempt to install latest node if not already

--- conf.d/node.fish
+++ conf.d/node.fish
@@ -6,9 +6,9 @@ end
 
 function _node_update --on-event node_install --on-event node_update
     set --function latest_version (fnm list-remote | tail -n1 | string replace --regex '^v' '')
-    fnm install $latest_version
-    fnm alias $latest_version latest
-    fnm use latest
+    if not contains v$latest_version (fnm list | awk '{print $2}')
+        fnm install $latest_version
+    end
 end
 
 function _node_uninstall --on-event node_uninstall
